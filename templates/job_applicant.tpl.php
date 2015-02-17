b39d5f5540b120c91481a43e52b2b318f9e05a09
rogerkuik <roger@dfgd.nl>
2015-02-17 15:12:32 +0100

    Various updates
    Variable translation
    Date/Time format
    Install / Uninstall variable

--- templates/job_applicant.tpl.php
+++ templates/job_applicant.tpl.php
@@ -3,5 +3,5 @@
     <li><?php print $lastname; ?></li>
     <li><?php print $mail; ?></li>
     <li><?php print $motivation; ?></li>
+    <li><?php print format_date($submitted, 'long'); // Date in long format ?></li>
 </ul>
-
